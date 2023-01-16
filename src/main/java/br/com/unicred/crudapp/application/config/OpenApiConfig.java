0aa4198c1549636520db7dd72ed1f4090d6bc753
Bruna Colin Baldin e Silva <brunacbaldin@gmail.com>
2023-01-16 17:02:01 -0300

    criada classe OpenApiConfig

---
+++ src/main/java/br/com/unicred/crudapp/application/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package br.com.unicred.crudapp.application.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Crud App").description("Documentação CrudApp API"));
+    }
+}
